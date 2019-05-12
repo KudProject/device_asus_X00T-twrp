a10d2ad11f9adeebaa71da5eafacbe2812c06d95
Albert I <kras@raphielgang.org>
2019-05-12 20:28:47 +0800

    X00T: Override several props to allow stock ROM flashing
    * Build system doesn't (seem to?) override PRODUCT_DEVICE, so it has to be
      manually patched here.
    * Stock firmware zip checks for ro.build.date.utc, which's false-positive
      since TWRP's build date might be newer than stock firmware that we're
      going to flash.
    * Also, just for kicks and giggles, override ro.bootimage.build.date.utc
      so we can have matching yet consistent POSIX build time.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -26,5 +26,15 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter X00T,$(TARGET_DEVICE)),)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+# Hack some props to allow stock ROM flashing
+BOARD_RECOVERY_IMAGE_PREPARE := \
+    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+    sed -i 's/ro.product.device=.*/ro.product.device=ASUS_X00TD/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
+
 endif
