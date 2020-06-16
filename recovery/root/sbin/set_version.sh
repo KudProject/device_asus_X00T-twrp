13e3f4bc4beb8667451a2cfa145abb59466c5bd8
Albert I <kras@raphielgang.org>
2020-06-16 19:41:23 +0800

    X00T: Run set_version on boot and don't write to persist
    We can't really predict until when persist is used for decryption; waiting
    for instance where adbd gets killed and restarted by TWRP isn't really
    accurate. In case of no property on currently installed firmware, fallback
    to default properties specified by this script.
    
    This way, we don't need to touch one of the most dangerous partition.
    
    Also, exit script upon error.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- recovery/root/sbin/set_version.sh
+++ recovery/root/sbin/set_version.sh
@@ -1,19 +1,27 @@
 #!/sbin/sh
+# Copyright (C) 2019-2020 KudProject Development
+# SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0
+
+set -e
+
+asus_sku=WW
+asus_version=16.2017.2005.082
 
 mount -t ext4 /dev/block/bootdevice/by-name/system /system
-mount -t ext4 /dev/block/bootdevice/by-name/persist /persist
 
 asus_prop=$(grep -E 'asus.sku|asus.version' /system/build.prop)
-
-[ -n "$asus_prop" ] && echo "$asus_prop" | sed -e 's/ /\n/' > /persist/asus.prop
-
-if [ -f /persist/asus.prop ]; then
-    asus_sku=$(grep sku /persist/asus.prop | cut -d '=' -f 2)
-    [ -n "$asus_sku" ] && setprop ro.build.asus.sku "$asus_sku"
-
-    asus_version=$(grep version /persist/asus.prop | cut -d '=' -f 2)
-    [ -n "$asus_version" ] && setprop ro.build.asus.version "$asus_version"
+if [ -n "$asus_prop" ]; then
+    echo "$asus_prop" | sed -e 's/ /\n/' > /tmp/asus.prop
+    asus_sku=$(grep sku /tmp/asus.prop | cut -d'=' -f2)
+    asus_version=$(grep version /tmp/asus.prop | cut -d'=' -f2)
+    rm /tmp/asus.prop
+else
+    echo "[kp] custom firmware detected or no ASUS version found; setting default values" >> /tmp/recovery.log
 fi
 
-umount /persist
+setprop ro.build.asus.sku "$asus_sku"
+setprop ro.build.asus.version "$asus_version"
+
 umount /system
+
+echo "[kp] setting ASUS version done." >> /tmp/recovery.log
