345b8e1d23a60541e1f31c194b43dc1693abb8a7
Albert I <kras@raphielgang.org>
2021-11-25 11:47:07 +0800

    X00T: Update `set_version.sh` for Android 10+ based TWRP
    Signed-off-by: Albert I <kras@raphielgang.org>

--- recovery/root/system/bin/set_version.sh
+++ recovery/root/system/bin/set_version.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
-# Copyright (C) 2019-2020 KudProject Development
+# Copyright (C) 2019-2021 KudProject Development
+#
 # SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0
 
 asus_sku=WW
@@ -7,10 +8,10 @@ asus_version=16.2017.2009.087
 
 recLog() { echo "[kp] $1" >>/tmp/recovery.log; }
 
-mount -t ext4 /dev/block/bootdevice/by-name/system /system
+mkdir /s && mount -t ext4 /dev/block/bootdevice/by-name/system /s
 
-[ -f "/system/system/build.prop" ] && system_root=/system
-asus_prop=$(grep -E 'asus.sku|asus.version' $system_root/system/build.prop 2>/dev/null)
+[ -f "/s/system/build.prop" ] && system_root=/system
+asus_prop=$(grep -E 'asus.sku|asus.version' /s$system_root/build.prop 2>/dev/null)
 if [ -n "$asus_prop" ]; then
     echo "$asus_prop" | sed -e 's/ /\n/' > /tmp/asus.prop
     asus_sku=$(grep sku /tmp/asus.prop | cut -d'=' -f2)
@@ -23,6 +24,6 @@ fi
 setprop ro.build.asus.sku "$asus_sku"
 setprop ro.build.asus.version "$asus_version"
 
-umount /system
+umount /s && rm -r /s
 
 recLog "setting ASUS version done"
