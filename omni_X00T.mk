6669ac052ae774016e10e99e216387c688ce4376
Albert I <kras@raphielgang.org>
2021-11-25 19:57:23 +0800

    X00T: Drop `ro.hardware.keystore` property
    Unnecessary for FBE.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- omni_X00T.mk
+++ omni_X00T.mk
@@ -22,9 +22,6 @@ $(call inherit-product, build/target/product/aosp_base.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.keystore=sdm660
-
 # APEX libraries
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
