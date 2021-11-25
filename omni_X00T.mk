16c8d70d8c7cf21bf8d0a987c9a93db98cd13742
Albert I <kras@raphielgang.org>
2021-11-25 11:02:55 +0800

    X00T: Switch to `qcom_decrypt_fbe` for data decryption
    Also fix wrong flag: TARGET_INCLUDE_FBE -> TARGET_INCLUDE_CRYPTO_FBE.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- omni_X00T.mk
+++ omni_X00T.mk
@@ -29,6 +29,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
+# Crypto
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
 # Soong namespace
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
